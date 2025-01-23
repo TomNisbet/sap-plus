6800e85ab3ce9b07b25ceebf86a2d2cb17516102
Tom Nisbet <t.talk@nisbethome.com>
2025-01-22 23:41:45 -0500

    assembler and test program updates

--- docs/support/prime.asm
+++ docs/support/prime.asm
@@ -1,7 +1,12 @@
+; when doing looping to find divisors, stop if DIV is more than NUM/2.  Don't have an easy way to
+; compute NUM/2, but can do DIV*2 instead and compare to NUM.  For example, there is no need to test
+; past 11 to know that 23 is prime.  No reason to divide by 13, 17, and 19.
+
+x123    equ      222
         data
 NUM:    byte            ; number value to be tested for prime
 DIV:    byte            ; current divisor value for prime test
-DIVIX:  byte            ; index in the primes array of DIV
+DIVIX:  byte            ; index in the primes array of DIV    ***  NOT NEEDED????? ***
 ENDIX:  byte            ; address of most recently found prime
 
         data    0xff
@@ -15,22 +20,23 @@ START:
         ina             ; start with 2 as the first number to test for prime
         sam     NUM
 
-        lai     PRIMES  ; initialize the current and last prime to the list start
-        sam     DIVIX ; --- IS DIVIX NEEDED or can it be SP
+        lai     PRIMES  ; initialize the current and last prime to be the first and only entry
+        sam     DIVIX ; --- IS DIVIX NEEDED or can it just be SP
         sam     ENDIX
 NXTPRM:
         ; Check if the current value of NUM is prime by dividing it by all primes smaller than NUM/2
         lai     PRIMES  ; No stack needed, so use the SP as the pointer into the PRIMES array
-        dca
-        tas
+        tas             ; The first prime is one, but PLA decrements SP before reading, so the first
+                        ;   DIV used will be the second prime (which won't exist the first time through)
 NXTDIV:
         ; Check if NUM is prime by dividing it by the current prime value pointed to by SP
-        tsa             ; If SP < ENDIX (counts down) then no more primes to test against, so NUM is prime
+        tsa             ; If SP = ENDIX (counts down) then no more primes to test against, so NUM is prime
         cpm     ENDIX
-        jc      ISPRM   ; Found one!
-        pla             ; Get divisor from primes list; also decrements SP to be ready to grab the next DIV
-        sam     DIV     ; If DIV > 127 or DIV > (NUM/2) then it is too big to evenly divide any possible value
-        asl             ;   of NUM.  This means that all useful DIVs have been tested, so NUM is prime.
+        jz      ISPRM   ; SP = ENDIX
+        pla             ; Decrement SP and get next divisor from primes list.  Store it in DIV.
+        sam     DIV     ; If DIV > 127 or DIV > (NUM/2) then it is too big to evenly divide any possible
+        asl             ;   value of NUM.  This means that all useful DIVs have been tested, so NUM is prime.
+                        ; Note there is no need to check DIV=(NUM/2) because that would have been caught when DIV=2.
         jc      ISPRM   ; DIV > 127
         cpm     NUM
         jc      ISPRM   ; DIV > (NUM/2)
@@ -41,18 +47,15 @@ ISDIV:
         jz      NEXT    ; zero means it is divisible, so this number is not prime
         jnc     NXTDIV  ; carry clear indicates underflow, not evenly divisible, try the next DIV
         jmp     ISDIV   ; division not done, keep subtracting
-;when doing looping to find divisors, stop if the div is more than a/2.  Don't have an a/2 operator
-;but can do div*2 instead ad compare to A.  e.g. once we hit 11, then 23 is known prime.  No need
-;to try 13, 17, and 19.
 
 ISPRM:
         lam     ENDIX   ; point SP to the end of the PRIMES list
+        dca             ;   and update it to add a new entry
+        sam     ENDIX
         tas
         lam     NUM     ; output NUM and add it to the end of the list
         out
-        pha             ; bumps the SP down to the next entry and store NUM
-        tsa             ; update ENDIX with new SP value
-        sam     ENDIX
+        pha             ; store NUM in the next entry in the PRIMES array and decrement SP
 
 NEXT:
         lam     NUM     ; check the next highest number for prime
@@ -64,6 +67,6 @@ NEXT:
 SPIN:
         lai     0x55    ; Loop here forever, but at least look cool while doing it
 SPIN2:
-        not
+        cyn
         jmp     SPIN2
 
--- docs/support/wwasm.py
+++ docs/support/wwasm.py
@@ -3,10 +3,11 @@
 
 import argparse
 
-__version__="1.0"
+__version__="1.1"
 
 parser = argparse.ArgumentParser("World's Worst Assembler")
 parser.add_argument('-c', '--code', action='store_true', help="write C code for Arduino Loader")
+parser.add_argument('-l', '--list', action='store_true', help="write a human-readale listing - don't use with -c")
 parser.add_argument('-m', '--monitor', action='store_true', help="write Insert commands for Monitor CLI")
 parser.add_argument("filename", help="Input file name")
 args = parser.parse_args()
@@ -16,12 +17,11 @@ ins = {'NOP': '00', 'OUT': '01', 'LAI': '02', 'LAM': '03', 'SAM': '04', 'TAS': '
        'JMP': '10', 'JC':  '11', 'JZ':  '12', 'JNC': '13', 'JNZ': '14', 'PHA': '15', 'PLA': '16', 
        'JSR': '17', 'RTS': '18', 'RC':  '19', 'RZ':  '1a', 'RNC': '1b', 'RNZ': '1c', 
        'ADI': '20', 'ADM': '21', 'SBI': '22', 'SBM': '23', 'ACI': '24', 'ACM': '25', 'SCI': '26', 
-       'SCM': '27', 'CPI': '28', 'CPM': '29'}
+       'SCM': '27', 'CPI': '28', 'CPM': '29', 'CYN': '3f' }
 
 def assemble(pass2):
     code = 0
     data = 0
-    mem = {}
     memAddr = 0
     mem[memAddr]= []
     isCode = True
@@ -29,78 +29,95 @@ def assemble(pass2):
     with open(args.filename, 'r') as asmFile:
         for line in asmFile:
             lineNum += 1
+            outLine = None
+            listLine = None
             label = None
+            printLabel = ""
             comment = None
+            if len(line.strip()) == 0:
+                # preserve empty lines in list file
+                listLine = ""
             ix = line.find(';')
             if ix >= 0:
                 comment = line[ix+1:].rstrip()
                 line = line[:ix]
-                if pass2 and args.code and not len(line.strip()):
-                    print("//{}".format(comment))
+                if pass2 and not len(line.strip()):
+                    # line contains only a comment
+                    outLine = "//{}".format(comment)
+                    listLine = ";{}".format(comment)
+
             parts = line.split()
-            if not parts:
-                continue
-
-            if len(parts) == 3 and parts[1].lower() == 'equ':
-                # Special case because the label in an EQU does not have a colon.  The later
-                # command processing code will add the value to the lables table
-                label = parts.pop(0).upper()
-            elif parts[0][-1] == ':':
-                label = parts.pop(0)[0:-1].upper()
-                if not pass2:
-                    labels[label] = code if isCode else data
-            op = parts.pop(0).upper() if parts else None
-            arg = parts.pop(0).upper() if parts else None
-            argVal = None
-            if arg:
-                if arg[0].isdigit():
-                    argVal = int(arg, 0)
-                else:
-                    argVal = labels.get(arg)
-                    if argVal == None:
-                        if pass2:
-                            print("{}: label not found: {}".format(lineNum, arg))
-                        else:
-                            argVal = 0 # don't fail a missing label on pass 1
-            #if asmPass == 1:
-            #    print("label={} op={} arg={}".format(label, op, arg))
-
-            outLine = None
-            if not op:
-                continue
-            elif op == 'DATA':
-                isCode = False
-                data = argVal if arg else data
-            elif op == 'CODE':
-                isCode = True
-                code = argVal if arg else code
-                memAddr = code
-                mem[memAddr] = []
-            elif op == 'EQU':
-                labels[label] = argVal
-            elif op == 'BYTE':
-                if isCode:
-                    code += 1
-                else:
-                    data += 1
-            else:
-                h = ins[op]
-                mem[memAddr].append(h) 
+            if parts:
+                if len(parts) == 3 and parts[1].lower() == 'equ':
+                    # Special case because the label in an EQU does not have a colon.  The later
+                    # command processing code will add the value to the labels table
+                    label = parts.pop(0).upper()
+                    printLabel = label
+                elif parts[0][-1] == ':':
+                    label = parts.pop(0)[0:-1].upper()
+                    printLabel = label+':'
+                    if not pass2:
+                        # store label in the table if pass 1
+                        labels[label] = code if isCode else data
+                op = parts.pop(0).upper() if parts else None
+                arg = parts.pop(0).upper() if parts else None
+                argVal = None
                 if arg:
-                    outLine = "    N_{:8} {},".format(op+',', hex(argVal))
-                    mem[memAddr].append(format(argVal, '02X')) 
-                    code += 2
+                    if arg[0].isdigit():
+                        argVal = int(arg, 0)
+                    else:
+                        argVal = labels.get(arg)
+                        if argVal == None:
+                            if pass2:
+                                print("{}: label not found: {}".format(lineNum, arg))
+                            else:
+                                argVal = 0 # don't fail a missing label on pass 1
+                #if asmPass == 1:
+                #    print("label={} op={} arg={}".format(label, op, arg))
+                if not op:
+                    if printLabel:
+                        listLine = "           {}".format(printLabel)
+                elif op == 'DATA':
+                    isCode = False
+                    data = argVal if arg else data
+                elif op == 'CODE':
+                    isCode = True
+                    code = argVal if arg else code
+                    memAddr = code
+                    mem[memAddr] = []
+                elif op == 'EQU':
+                    labels[label] = argVal
+                    listLine = "{:02X}          {:8}{:8} {}".format(argVal, printLabel, op, arg)
+                elif op == 'BYTE':
+                    if isCode:
+                        listLine = "{:02X}:         {:8}{:8}".format(code, printLabel, op)
+                        code += 1
+                    else:
+                        listLine = "{:02X}:         {:8}{:8}".format(data, printLabel, op)
+                        data += 1
                 else:
-                    outLine = "    N_{},".format(op)
-                    code += 1
+                    h = ins[op]
+                    mem[memAddr].append(h) 
+                    if arg:
+                        outLine = "    N_{:8} {},".format(op+',', hex(argVal))
+                        listLine = "{:02X}: {} {:02X}   {:8}{:8} {}".format(code, h, argVal, printLabel, op, arg)
+                        mem[memAddr].append(format(argVal, '02X')) 
+                        code += 2
+                    else:
+                        outLine = "    N_{},".format(op)
+                        listLine = "{:02X}: {}      {:8}{:8}".format(code, h, printLabel, op)
+                        code += 1
 
             if outLine and args.code and pass2:
                 if comment:
-                    outLine = "{:24} //{}".format(outLine, comment)
+                    outLine = "{:24}//{}".format(outLine, comment)
                 print(outLine)
 
-    if pass2 and args.monitor:
-        printMem(mem)
+            if listLine != None and args.list and pass2:
+                if comment:
+                    listLine = "{:36};{}".format(listLine, comment)
+                print(listLine)
+
 
 def printMem(mem):
     step = 8
@@ -112,6 +129,10 @@ def printMem(mem):
 
 
 labels = {}
+mem = {}
 assemble(False)
 assemble(True)
 #print(labels)
+if args.monitor:
+    printMem(mem)
+
